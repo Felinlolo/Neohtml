f808191776fd6b910450b9c94a8d37a1594fa96a
Félinlolo <felinlolo2007@gmail.com>
2022-07-28 11:25:47 +0200

    pop up

--- pop_up_info.js
+++
@@ -1,9 +0,0 @@
-var sidenav = document.getElementsById("pop_up");
-var close = document.getElementsById("close");
-
-
-close.onclick = closeNav;
-
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-    getElementsById('info')[0].style.opacity=0}
